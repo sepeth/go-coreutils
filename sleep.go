e890c33f59b3ff8098566c640a04cfe725d414da
Huseyin Oguz Albayrak <huseyinalb@Huseyins-MacBook-Air.local>
2013-08-10 18:44:24 +0300

    added sleep

---
+++ sleep.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "flag"
+    "os"
+    "strconv"
+    "time"
+)
+
+func usage() {
+    fmt.Println("Usage: sleep seconds")
+}
+
+func main () {
+    flag.Parse()
+    if len(flag.Args()) != 1 {
+        usage()
+        os.Exit(1)
+    }
+    i, err := strconv.Atoi(flag.Arg(0))
+    if err != nil {
+        usage()
+        os.Exit(1)
+    }
+    time.Sleep(time.Duration(i) * time.Second)
+}
